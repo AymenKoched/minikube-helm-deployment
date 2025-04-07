f2184cbe17c9fbfefe2a0cc0255cfa34f9bdf89b
Aymen Koched <aymen.koched@scaylab.fr>
2025-04-07 17:11:25 +0100

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:7000/api/message')
+            .then(response => setMessage(response.data.message))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
