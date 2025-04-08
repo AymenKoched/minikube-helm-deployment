a81ea8d43ce2906bd71abdd397bbe972a623cf9a
Aymen Koched <aymen.koched@scaylab.fr>
2025-04-08 13:47:57 +0100

    feat: test filter

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 7001 ;
 
 app.use(cors());
 
@@ -10,6 +9,7 @@ app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
 
+const port = 7001 ;
 app.listen(port, () => {
     console.log(`Backend server running at http://localhost:${port}`);
 });
