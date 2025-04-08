606abb96864f41b2a8ae46f407ea00ca52da981c
Aymen Koched <aymen.koched@scaylab.fr>
2025-04-08 16:23:02 +0100

    fix: add ingress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        axios.get(`http://backend-service:7001/api/message`)
+        axios.get(`http://my-demo/api/message`)
             .then(response => setMessage(response.data.message))
             .catch(error => console.error('Error fetching data:', error));
     }, []);
